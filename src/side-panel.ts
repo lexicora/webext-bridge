62a9f9523652406df635e78ff4fb7fced7cbb95a
Timothy Maurer <timothygmaurer@outlook.com>
2026-01-08 20:09:03 +0100

    Add commented-out alternative approach

--- src/side-panel.ts
+++ src/side-panel.ts
@@ -20,3 +20,23 @@ export function getSidePanel(tabId) {
     onOpenStreamChannel,
   };
 }
+
+// Credit to code in this file: https://github.com/serversideup/webext-bridge/discussions/91 -> https://github.com/adao [Author]
+
+// Second approach:
+// import browser from 'webextension-polyfill'
+// import { createEndpointRuntime } from './internal/endpoint-runtime'
+// import { createStreamWirings } from './internal/stream'
+// import { createPersistentPort } from './internal/persistent-port'
+
+// const [tab] = await browser.tabs.query({active: true, currentWindow: true})
+// const port = createPersistentPort(`side-panel@${tab.id}`)
+// const endpointRuntime = createEndpointRuntime(
+//   'side-panel',
+//   message => port.postMessage(message),
+// )
+
+// port.onMessage(endpointRuntime.handleMessage)
+
+// export const { sendMessage, onMessage } = endpointRuntime
+// export const { openStream, onOpenStreamChannel } = createStreamWirings(endpointRuntime)
